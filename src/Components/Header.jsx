67163ccbd4d93352ca5d0385d2c35b91cebf5d94
TechgicusSoftware <TechgicusAppwrite@gmail.com>
2025-05-27 17:48:22 +0530

    addchanges

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaHome, FaInfoCircle, FaServicestack, FaPhoneAlt } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
-import logo from "../Assets/logo.svg";
+import logo from "../Assets/logo1.png";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
